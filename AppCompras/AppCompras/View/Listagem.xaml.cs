67877438d8d0effe89690565852acbe2864a6bd2
mahteixeira <teixeiramarina13@gmail.com>
2022-11-09 15:34:26 -0300

    Adicionar arquivos de projeto.

---
+++ AppCompras/AppCompras/View/Listagem.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCompras.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listagem : ContentPage
+    {
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+
+        private void Novo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new NovoProduto());
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Somar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
